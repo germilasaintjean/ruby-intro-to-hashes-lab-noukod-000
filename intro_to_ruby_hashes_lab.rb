a7acbe12588d896d62cbb6d37a1893069ba3a1b4
Saint-Jean Germila <germila@noukod.com>
2018-10-02 18:31:37 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -22,9 +22,8 @@ def monopoly_with_second_tier
 	monopoly= {
 					 :railroads => {
 						 :pieces => 4,
-						 :rent_in_dollars => {},
-						 :names => {},
-						 :shortline => {}
+						 :rent_in_dollars => {}
+						 
 					 }
 					 }
 
